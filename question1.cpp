cc5839ae17c6fa0e592c6c0c8e8ac95610b9669f
Vartika Yaduvanshi <vv1415@srmist.edu.in>
2021-12-15 08:59:15 +0530

    question updated

--- question1.cpp
+++ question1.cpp
@@ -1,3 +1,28 @@
+ABSTRACT CLASS & VIRTUAL FUNCTION
+Ravindran is working in afamous Multinational IT Firm. He has been recently assigned the task of colecting 
+salary details of the employees in the company.
+The company has two categories of Employees namely Developer and Driver.
+The final statistics needs to be submitted to the CEO of the compant today.
+Since the number of people working in the firm is huge Ravindran is finding it difficult to format the data,
+
+Test Case 1                             
+INPUT (STDIN)
+10465
+4325
+EXPECTED OUTPUT
+Salary of Developer:10465
+Salary of Driver:4325
+	
+Test Case 2
+INPUT (STDIN)
+17231
+6390
+EXPECTED OUTPUT
+Salary of Developer:17231
+Salary of Driver:6390
+	
+	
+	
 #include <iostream>
 using namespace std;
 
